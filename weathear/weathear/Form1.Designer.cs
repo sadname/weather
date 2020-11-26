f9dbc78a4108f423674d174df83369e352abbd16
Andrew Dvoikyn <56105820+sadname@users.noreply.github.com>
2020-11-26 13:36:23 +0300

    Add files via upload

---
+++ weathear/weathear/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace weathear
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lCity = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lTemperatura = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(55, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(659, 203);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(174, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Город";
+            // 
+            // lCity
+            // 
+            this.lCity.AutoSize = true;
+            this.lCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lCity.Location = new System.Drawing.Point(388, 233);
+            this.lCity.Name = "lCity";
+            this.lCity.Size = new System.Drawing.Size(60, 24);
+            this.lCity.TabIndex = 1;
+            this.lCity.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(174, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Температура";
+            // 
+            // lTemperatura
+            // 
+            this.lTemperatura.AutoSize = true;
+            this.lTemperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lTemperatura.Location = new System.Drawing.Point(388, 277);
+            this.lTemperatura.Name = "lTemperatura";
+            this.lTemperatura.Size = new System.Drawing.Size(60, 24);
+            this.lTemperatura.TabIndex = 1;
+            this.lTemperatura.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lTemperatura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lCity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lTemperatura;
+    }
+}
+
---
+++ weathear/weathear/Form1.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace weathear
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string url = "https://api.openweathermap.org/data/2.5/weather?q=Donetsk&appid=0ade4ff8fdb2d9a0ceec38baa9fe25c8&units=metric";
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+			HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+			StreamReader reader = new StreamReader(res.GetResponseStream());
+			string response = reader.ReadToEnd();
+			richTextBox1.Text = response;
+			WeatherResponse wr = JsonConvert.DeserializeObject<WeatherResponse>(response);
+			lCity.Text = wr.Name;
+			lTemperatura.Text = wr.Main.Temp.ToString();
+		}
+	}
+}
